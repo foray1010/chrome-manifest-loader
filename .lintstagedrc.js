8380fa07ebbe8ca1a3358a063b82fd61d2f696c1
Alex Young <foray1010@users.noreply.github.com>
2020-04-01 11:03:44 +0800

    fixup! chore(deps): update dependency @foray1010/remark-preset to v3

---
+++ .lintstagedrc.js
@@ -0,0 +1,21 @@
+'use strict'
+
+module.exports = {
+  '*.{json,yaml,yml}': (filenames) => {
+    const commands = []
+
+    filenames
+      .filter((name) => /\/package\.json$/.test(name))
+      .forEach((name) => {
+        commands.push(`sort-package-json ${name}`)
+      })
+
+    commands.push(`yarn prettier --write ${filenames.join(' ')}`)
+
+    return commands
+  },
+  '*.js': ['yarn prettier --write', 'yarn eslint --fix'],
+  '*.md': (filenames) => {
+    return [`yarn prettier --write ${filenames.join(' ')}`, 'yarn remark .']
+  },
+}
