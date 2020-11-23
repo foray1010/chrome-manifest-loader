2b742010686ad8a1d395361dd9cde90458139cf1
Alex Young <foray1010@users.noreply.github.com>
2020-11-23 14:35:30 +0800

    chore: update lint-staged config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,9 @@
 'use strict'
 
 module.exports = {
+  '*.{cjs,js,mjs,ts,tsx}': ['yarn prettier --write', 'yarn eslint --fix'],
   '*.{json,yaml,yml}': 'yarn prettier --write',
-  '*.js': ['yarn prettier --write', 'yarn eslint --fix'],
-  '*.md': (filenames) => {
+  '*.{markdown,md}'(filenames) {
     return [`yarn prettier --write ${filenames.join(' ')}`, 'yarn remark .']
   },
 }
